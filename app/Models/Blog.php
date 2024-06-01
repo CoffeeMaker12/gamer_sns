3e1931c6f46124569b6b97a80471cff15ce65d0b
CoffeeMaker12 <67kota2001@gmail.com>
2024-06-01 20:28:30 +0900

    モデル定義途中

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -8,4 +8,25 @@ use Illuminate\Database\Eloquent\Model;
 class Blog extends Model
 {
     use HasFactory;
+    
+    protected $fillable = [
+        'title',
+        'body',
+        'user_id'
+    ];
+    
+    use HasFactory;
+    use SoftDeletes;
+    
+    //「1対多」の関係なので単数系
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    
+    //「多対多」
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class);
+    }
 }
--- app/Models/Board.php
+++ app/Models/Board.php
@@ -7,5 +7,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Board extends Model
 {
+    protected $fillable = [
+        'title',
+        'body',
+        'user_id'
+    ];
+    
     use HasFactory;
+    use SoftDeletes;
+    
+    //「1対多」の関係なので単数系
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    
+    //「1対多」
+    public function board_comments()
+    {
+        return $this->hasMany(BoardComment::class);
+    }
 }
